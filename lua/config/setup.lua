b794b3bedc67fcb8073c4128fc2692f5d3094100
Alec Rospierski <alec.rospierski@stellantis.com>
2025-10-28 12:37:13 -0400

    add harpoon to config

--- lua/config/setup.lua
+++ lua/config/setup.lua
@@ -11,24 +11,24 @@ local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
 -- changes selecting autocomplete to using tabs
 cmp.setup({
-  mapping = {
-    ["<CR>"] = cmp.mapping.confirm({ select = false }),
-    ["<Tab>"] = cmp.mapping.select_next_item(cmp_select),
-    ["<S-Tab>"] = cmp.mapping.select_prev_item(cmp_select),
-  },
+    mapping = {
+        ["<CR>"] = cmp.mapping.confirm({ select = false }),
+        ["<Tab>"] = cmp.mapping.select_next_item(cmp_select),
+        ["<S-Tab>"] = cmp.mapping.select_prev_item(cmp_select),
+    },
 })
 
 -- harpoon setup
 
--- local ui = require('harpoon.ui')
--- local mark = require('harpoon.mark')
--- -- local cmd_ui = require('harpoon.cmd-ui')
---
--- vim.keymap.set("n", "<leader>ha", function() mark.add_file() end)
--- vim.keymap.set("n", "<leader>hm", function() ui.toggle_quick_menu() end)
--- vim.keymap.set("n", "<leader>hp", function() ui.nav_prev() end)
--- vim.keymap.set("n", "<leader>hn", function() ui.nav_next() end)
--- vim.keymap.set("n", "<leader>hq", function() ui.nav_file(1) end)
--- vim.keymap.set("n", "<leader>hw", function() ui.nav_file(2) end)
--- vim.keymap.set("n", "<leader>he", function() ui.nav_file(3) end)
--- vim.keymap.set("n", "<leader>hr", function() ui.nav_file(4) end)
+local harpoon = require("harpoon")
+
+harpoon:setup()
+
+vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end)
+vim.keymap.set("n", "<leader>hm", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+vim.keymap.set("n", "<leader>hp", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<leader>hn", function() harpoon:list():next() end)
+vim.keymap.set("n", "<leader>hq", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<leader>hw", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<leader>he", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<leader>hr", function() harpoon:list():select(4) end)
